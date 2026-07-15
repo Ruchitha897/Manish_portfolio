acc5aa2b69201ffc3eee78f68a59e390b15655c3
Ruchitha897 <ruchithalagala@gmail.com>
2026-07-15 21:05:38 +0530

    Fix unused import TypeScript errors crashing Vercel build

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
-import { useRef, useEffect } from 'react';
-import { motion, useScroll, useTransform, useInView, animate } from 'framer-motion';
+import { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 
 export default function About() {
